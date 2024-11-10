dc94eaa407b9c5c7375a8de2fbf3291c9490ff43
Guillermo Felipe Martin Gonzalez <53008413+gfmg@users.noreply.github.com>
2024-11-10 12:39:36 +0000

    Task 6. Finalising script

--- weather.sh
+++ weather.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
+echo "## Downloading weather data from Met Eireann: "
+date
 wget -O data/weather/`date +"%Y%m%d_%H%M%S.json"` https://prodapi.metweb.ie/observations/athenry/today 
+echo "## Weather data downloaded"
