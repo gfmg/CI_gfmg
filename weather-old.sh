c42f6f84224ee9ae2196401ce4b3dd38105e5a17
Guillermo Martin <gfmg1992@hotmail.com>
2024-12-10 11:49:36 +0000

    Removing old weather.sh

--- weather-old.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-echo "## Downloading weather data from Met Eireann: "
-date
-wget -O data/weather/`date +"%Y%m%d_%H%M%S.json"` https://prodapi.metweb.ie/observations/athenry/today 
-echo "## Weather data downloaded"
